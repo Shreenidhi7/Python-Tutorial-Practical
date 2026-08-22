ad8be6df7b65a175b82d612f3b1a8aaa5e927a99
Shreenidhi7 <shreenidhisharma7@gmail.com>
2026-08-22 12:10:41 +0530

    practice - reverse_string -220826

--- PythonTTDTutorial/0-InterviewCodingProgramsPractice/Section1-String Programs/1-ReverseAString.py
+++ PythonTTDTutorial/0-InterviewCodingProgramsPractice/Section1-String Programs/1-ReverseAString.py
@@ -34,30 +34,30 @@
 ###################################################
 ### Practice
 def reverse_string(s: str) -> str:
-    #1. Convert the string into a list of characters
-    characters = list(s)
+    #1. Turn the strings into a list of character blocks
+    character = list(s)
 
-    #2. Set the starting points
+    #2. Set up our starting points(positions)
     left_index = 0
-    right_index = len(characters) - 1
+    right_index = len(character) - 1
 
-    #3. Start the Loop
+    #3. Start the loop
     while left_index < right_index:
-        #1. Save the letters in "hodling boxes"
-        left_letter = characters[left_index]
-        right_letter = characters[right_index]
+        #Save letters in temporary holding boxes, so that we don't lose them
+        left_letter = character[left_index]
+        right_letter = character[right_index]
 
-        #2. Swap the letter using the holding boxes
-        characters[left_index] = right_letter
-        characters[right_index] = left_letter
+        #Perform Swap using our holding boxes
+        character[left_index] = right_letter
+        character[right_index] = left_letter
 
-        #3. Move the pointers to the middle
+        #Move the pointers(positions) to the middle, one at a time
         left_index = left_index + 1
-        right_index = right_index - 1
+        right_index = right_index -1
 
-    #4. Return the reversed string
-    reversed_string = ''.join(characters)
+    #4. Glue it all back together "After" the loop is completely done
+    reversed_string = "".join(character)
     return reversed_string
 
+print(reverse_string("automation"))
 print(reverse_string("racecar"))
-print(reverse_string("malayalam"))
